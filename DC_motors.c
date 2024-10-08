06844ab58b5871c21acc04ae1d99ba49b4f0f595
Md Shamim Ahmad <56692841+md-shamim-ahmad@users.noreply.github.com>
2024-10-08 09:38:05 +0600

    Update DC_motors.c

--- DC_motors.c
+++ DC_motors.c
@@ -1,8 +1,16 @@
 void main() {
-     TrisB = 0x00;
-     TrisD = 0xff;
+     trisb = 0x00;
+     trisd = 0x00;
      portb = 0x00;
-     while(1){
+     while(1) {
+              if (portd.f0 == 0xff) {
+                 Delay_ms(200);
+                 portd.f0 = 0x00;
+                 portb.f2 = 0xff;
+              } else if(portd.f0 == 0x00){
+                 Delay_ms(200);
+                 portb.f2 = 0x00;
+              }
               portb.f0 = 0xff;
               portb.f1 = 0x00;
      }
